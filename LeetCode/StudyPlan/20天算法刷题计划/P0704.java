1684904583e629dca70f43f8a1d77363911fc5a7
aqlzh <lzh_create@qq.com>
2021-08-17 14:00:01 +0800

    feat (20 天计划)

---
+++ LeetCode/StudyPlan/20天算法刷题计划/P0704.java
@@ -0,0 +1,22 @@
+package leetcodePlan;
+
+public class P0704 {
+
+	public static void main(String[] args) {
+		 
+         // ÊäÈë  nums = [-1,0,3,5,9,12], target = 9
+		int [] nums = {-1,0,3,5,9,12} ;
+		int target = 9 ;
+		
+		System.out.println(search(nums,target));
+	}
+	
+	 public static int search(int[] nums, int target) {
+         for(int i =0 ; i <nums.length ;i++){
+               if(nums[i]==target) return i ;
+         }
+          
+               return -1  ;
+    }
+
+}
