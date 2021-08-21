945989cfea058a80a9d1acacf9bb92dfd714fe13
aqlzh <lzh_create@qq.com>
2021-08-21 22:50:06 +0800

    feat 21/8/21

---
+++ LeetCode/StudyPlan/20天算法刷题计划/第五天 P0876 链表的中间结点/Solution.java
@@ -0,0 +1,101 @@
+package leetcodePlan;
+
+    //   正确代码
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    public ListNode(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("arr can not be empty");
+        }
+        this.val = nums[0];
+        ListNode curr = this;
+        for (int i = 1; i < nums.length; i++) {
+            curr.next = new ListNode(nums[i]);
+            curr = curr.next;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        ListNode cur = this;
+        while (cur != null) {
+            s.append(cur.val);
+            s.append(" -> ");
+            cur = cur.next;
+        }
+        s.append("NULL");
+        return s.toString();
+    }
+}
+
+public class Solution {
+
+	
+	//  解法一 ： 快慢指针
+    public ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    
+    // 解法二 ： 借助数组
+    public ListNode middleNode2(ListNode head) {
+        ListNode[] A = new ListNode[100];
+        int t = 0;
+        while (head != null) {
+            A[t++] = head;
+            head = head.next;
+        }
+        return A[t / 2];
+    }
+    
+    
+    // 解法三 ： 单指针
+    
+    public ListNode middleNode3(ListNode head) {
+        int n = 0;
+        ListNode cur = head;
+        while (cur != null) {
+            ++n;
+            cur = cur.next;
+        }
+        int k = 0;
+        cur = head;
+        while (k < n / 2) {
+            ++k;
+            cur = cur.next;
+        }
+        return cur;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6};
+        // int[] arr = new int[]{1, 2, 3, 4, 5};
+        ListNode head = new ListNode(arr);
+        Solution solution = new Solution();
+        ListNode res = solution.middleNode(head);
+        ListNode res2 = solution.middleNode2(head);
+        ListNode res3 = solution.middleNode3(head);
+        System.out.println(res);
+        System.out.println(res2);
+        System.out.println(res3);
+    }
+}
+
+
